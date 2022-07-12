e1438592c2ab3d8329fcca084682089c41ed5506
Snider <snider@lt.hn>
2022-07-12 15:00:12 +0100

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +8,6 @@ import {Router} from '@angular/router';
 export class AppComponent {
   title = 'directory-browser';
 
-  constructor(public router: Router) {
-  }
-
-  public onPayloadReceived($event: any) {
-    console.log($event)
+  constructor() {
   }
 }
